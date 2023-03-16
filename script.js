fc8e1df716642de2465468d673300dee37506d17
MikeMinheere <99067548@mydavinci.nl>
2023-03-16 13:05:01 +0100

    final edit

--- script.js
+++ script.js
@@ -4,256 +4,224 @@ var gameList = [
         "price": 0.00,
         "genre": "FPS",
         "rating": 4,
-        "filter": 0,
-        "id": 0
+        "filter": 0
     },
     {
         "title": "Dota 2",
         "price": 0.00,
         "genre": "MOBA",
         "rating": 3,
-        "filter": 0,
-        "id": 1
+        "filter": 0
     },
     {
         "title": "Goose Goose Duck",
         "price": 4.99,
         "genre": "Action",
         "rating": 5,
-        "filter": 0,
-        "id": 2
+        "filter": 0
     },
     {
         "title": "Apex Legends",
         "price": 0.00,
         "genre": "FPS",
         "rating": 4,
-        "filter": 0,
-        "id": 3
+        "filter": 0
     },
     {
         "title": "PUBG: BATTLEGROUNDS",
         "price": 29.99,
         "genre": "FPS",
         "rating": 3,
-        "filter": 0,
-        "id": 4
+        "filter": 0
     },
     {
         "title": "Lost Ark",
         "price": 49.99,
         "genre": "Action",
         "rating": 1,
-        "filter": 0,
-        "id": 5
+        "filter": 0
     },
     {
         "title": "Grand Theft Auto V",
         "price": 29.99,
         "genre": "FPS",
         "rating": 3,
-        "filter": 0,
-        "id": 6
+        "filter": 0
     },
     {
         "title": "Call of Duty®: Modern Warfare® II | Warzone™ 2.0",
         "price": 19.99,
         "genre": "FPS",
         "rating": 3,
-        "filter": 0,
-        "id": 7
+        "filter": 0
     },
     {
         "title": "Team Fortress 2",
         "price": 0.00,
         "genre": "FPS",
         "rating": 5,
-        "filter": 0,
-        "id": 8
+        "filter": 0
     },
     {
         "title": "Rust",
         "price": 39.99,
         "genre": "Action",
         "rating": 3,
-        "filter": 0,
-        "id": 9
+        "filter": 0
     },
     {
         "title": "Unturned",
         "price": 0.00,
         "genre": "RPG",
         "rating": 1,
-        "filter": 0,
-        "id": 10
+        "filter": 0
     },
     {
         "title": "ELDEN RING",
         "price": 59.99,
         "genre": "RPG",
         "rating": 5,
-        "filter": 0,
-        "id": 11
+        "filter": 0
     },
     {
         "title": "ARK: Survival Evolved",
         "price": 10.00,
         "genre": "RPG",
         "rating": 1,
-        "filter": 0,
-        "id": 12
+        "filter": 0
     },
     {
         "title": "War Thunder",
         "price": 0.00,
         "genre": "Simulation",
         "rating": 2,
-        "filter": 0,
-        "id": 13
+        "filter": 0
     },
     {
         "title": "Sid Meier's Civilization VI",
         "price": 29.99,
         "genre": "Simulation",
         "rating": 3,
-        "filter": 0,
-        "id": 14
+        "filter": 0
     },
     {
         "title": "Football Manager 2023",
         "price": 59.99,
         "genre": "Simulation",
         "rating": 3,
-        "filter": 0,
-        "id": 15
+        "filter": 0
     },
     {
         "title": "Warframe",
         "price": 0.00,
         "genre": "Looter-shooter",
         "rating": 3,
-        "filter": 0,
-        "id": 16
+        "filter": 0
     },
     {
         "title": "EA SPORTS™ FIFA 23",
         "price": 59.99,
         "genre": "Sport",
         "rating": 1,
-        "filter": 0,
-        "id": 17
+        "filter": 0
     },
     {
         "title": "Destiny 2",
         "price": 0.00,
         "genre": "FPS",
         "rating": 5,
-        "filter": 0,
-        "id": 18
+        "filter": 0
     },
     {
         "title": "Red Dead Redemption 2",
         "price": 59.99,
         "genre": "RPG",
         "rating": 4,
-        "filter": 0,
-        "id": 19
+        "filter": 0
     },
     {
         "title": "Tom Clancy's Rainbow Six Siege",
         "price": 19.99,
         "genre": "Simulation",
         "rating": 3,
-        "filter": 0,
-        "id": 20
+        "filter": 0
     },
     {
         "title": "The Witcher 3: Wild Hunt",
         "price": 39.99,
         "genre": "RPG",
         "rating": 4,
-        "filter": 0,
-        "id": 21
+        "filter": 0
     },
     {
         "title": "Terraria",
         "price": 9.99,
         "genre": "Sandbox",
         "rating": 4,
-        "filter": 0,
-        "id": 22
+        "filter": 0
     },
     {
         "title": "Stardew Valley",
         "price": 14.99,
         "genre": "Sandbox",
         "rating": 4,
-        "filter": 0,
-        "id": 23
+        "filter": 0
     },
     {
         "title": "Left 4 Dead 2",
         "price": 9.99,
         "genre": "FPS",
         "rating": 4,
-        "filter": 0,
-        "id": 24
+        "filter": 0
     },
     {
         "title": "Don't Starve Together",
         "price": 5.09,
         "genre": "RPG",
         "rating": 3,
-        "filter": 0,
-        "id": 25
+        "filter": 0
     },
     {
         "title": "MIR4",
         "price": 19.99,
         "genre": "RPG",
         "rating": 3,
-        "filter": 0,
-        "id": 26
+        "filter": 0
     },
     {
         "title": "PAYDAY 2",
         "price": 9.99,
         "genre": "Action",
         "rating": 2,
-        "filter": 0,
-        "id": 27
+        "filter": 0
     },
     {
         "title": "Path of Exile",
         "price": 0.00,
         "genre": "RPG",
         "rating": 4,
-        "filter": 0,
-        "id": 28
+        "filter": 0
     },
     {
         "title": "Project Zomboid",
         "price": 14.99,
         "genre": "Simulation",
         "rating": 4,
-        "filter": 0,
-        "id": 29
+        "filter": 0
     },
     {
         "title": "Valheim",
         "price": 19.99,
         "genre": "Sandbox",
         "rating": 5,
-        "filter": 0,
-        "id": 30
+        "filter": 0
     },
     {
         "title": "DayZ",
         "price": 44.99,
         "genre": "Simulation",
         "rating": 3,
-        "filter": 0,
-        "id": 31
+        "filter": 0
     }
 ]
 var totalAmount = 0;
